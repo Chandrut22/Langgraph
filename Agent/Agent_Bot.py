1905617ce81707be37f52e779df856925d236829
Chandrut22 <chandrutd22@gmail.com>
2025-11-23 10:49:28 +0530

    Complete the agent 1 - ChatBot

---
+++ Agent/Agent_Bot.py
@@ -0,0 +1,29 @@
+from typing import TypedDict, List
+from langchain_core.messages import HumanMessage
+from langgraph.graph import StateGraph, START, END
+from langchain_google_genai import ChatGoogleGenerativeAI 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    message: List[str]
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+def process_message(state: AgentState) -> AgentState:
+    response = llm.invoke([HumanMessage(content=state["message"])])
+    print(f"AI: {response.content}")
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process_message)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+app = graph.compile()
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        break
+    app.invoke(AgentState(message=[user_input]))
